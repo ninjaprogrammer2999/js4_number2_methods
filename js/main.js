5bb2ac78856df7d15b5371effc8fa08a7b75a502
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-03-07 10:03:36 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,12 +4,12 @@ const myNumber = 100;
 const myFloat = 100.019838;
 const myString = "100";
 // isInteger method which checks whether the number is integer or not
-//console.log(Number.isInteger(myString));
+console.log(Number.isInteger(myString));
 //*******************************************************************
 /* parseFloat method = this method parses an argument and returns 
 a float point number. If a number cannot be parsed from argument, 
 it returns NaN. */
-//console.log(Number.parseFloat(myString));
+console.log(Number.parseFloat(myString));
 //*******************************************************************
 /* toFixed method = formats a number according to how many decimal 
 points provide as the parameter */
